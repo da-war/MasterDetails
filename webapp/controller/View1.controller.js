18023c90dcd86c4465d26e00bc280fd12e06b525
Rana Dawar Abdullah <reactnativekadena@gmail.com>
2024-11-11 11:19:24 +0000

    Filteration of orders

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,12 +1,18 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/ui/model/FilterType"
 ],
-function (Controller) {
-    "use strict";
+    function (Controller,Filter,FilterOperator,FilterType) {
+        "use strict";
 
-    return Controller.extend("com.masterdetail.masterdetails.controller.View1", {
-        onInit: function () {
-
-        }
+        return Controller.extend("com.masterdetail.masterdetails.controller.View1", {
+            onListPress: function (oEvent) {
+                console.log("hello pressy pressy")
+                const orderId=oEvent.getParameter('listItem').getBindingContext().getProperty("OrderID");
+                const oFilter= new Filter("OrderID",FilterOperator.EQ,orderId);
+                this.getView().byId("orderTable").getBinding("items").filter(oFilter,FilterType.Application);
+            },
+        });
     });
-});
